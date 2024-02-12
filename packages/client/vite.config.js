1fd863109fb3df8a6f41d2d1cda554cc5c50c1fc
Ophir Bucai <ophirbucai@gmail.com>
2024-02-12 17:30:26 +0200

    Add proxy for development mode (it works!)

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
     build: {
         outDir: '../server/public'
+    },
+    server: {
+        ...mode === 'development' ? {
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:3030',
+                    changeOrigin: true,
+                    secure: false,
+                    ws: true
+                }
+            }
+        } : null,
     }
-})
+}))
