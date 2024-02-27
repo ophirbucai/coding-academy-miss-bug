f0da3c463fc43630b4ac1bc280ed39ac51d4bea5
Ophir Bucai <ophirbucai@gmail.com>
2024-02-27 12:30:29 +0200

    Env config added

---
+++ packages/server/config/dev.js
@@ -0,0 +1,5 @@
+export default {
+  dbURL: 'mongodb://localhost:27017',
+  dbName: 'bug_db',
+  port: 3030
+}
---
+++ packages/server/config/prod.js
@@ -0,0 +1,4 @@
+export default {
+  dbURL: 'mongodb+srv://theUser:' + process.env.DB_PASS + '@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbName: 'bug_db'
+}
